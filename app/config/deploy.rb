86ecd8097e2ae922070e0d011ae56f7cf20af4eb
Pierre-Henri <phbasic@gmail.com>
2016-01-27 17:57:20 +0100

    Still troubles with folders rights

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -8,6 +8,7 @@ set :deploy_to,   "/var/www/jdhm-api"
 set :app_path,    "app"
 set :cache_warmup, false
 ssh_options[:port] = 2222
+set :webserver_user,   "www-data"
 
 
 set :repository,  "https://github.com/Pierre-Henri-Bourdeau/jdhm-api.git"
@@ -22,3 +23,18 @@ set  :keep_releases,  3
 
 
 set :writable_dirs, ["var/cache", "var/logs"]
+
+after "deploy:update_code", "folders_rights"
+
+
+namespace :jdhm do
+
+    task :folders_rights do
+        capifony_pretty_print "--> Change mode of var/cache & var/log"
+        run "sudo chown -R #{webserver_user} #{latest_release}/#{cache_path}"
+        run "sudo chmod -R 775 #{latest_release}/#{cache_path}"
+        run "sudo chown -R #{webserver_user} #{latest_release}/#{log_path}"
+        run "sudo chmod -R 775 #{latest_release}/#{cache_path}"
+        capifony_puts_ok
+    end
+end
