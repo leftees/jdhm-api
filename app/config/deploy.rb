057502c2fdd349f2baab89aedc2d2487689aff9c
Pierre-Henri <phbasic@gmail.com>
2016-01-27 17:42:35 +0100

    Fix in deploy

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -6,14 +6,14 @@ set :user,        "ph"
 set :use_sudo,    false
 set :deploy_to,   "/var/www/jdhm-api"
 set :app_path,    "app"
+set :cache_warmup, false
 ssh_options[:port] = 2222
 
-set :repository,  "git@github.com:Pierre-Henri-Bourdeau/jdhm-api.git"
+
+set :repository,  "https://github.com/Pierre-Henri-Bourdeau/jdhm-api.git"
 set :scm,         :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
 
 set :model_manager, "doctrine"
-# Or: `propel`
 
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain, :primary => true       # This may be the same as your `Web` server
