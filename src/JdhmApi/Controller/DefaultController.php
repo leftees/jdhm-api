524ccc5dfd8316864c1f3ddcef0f34920521029a
Pierre-Henri <phbasic@gmail.com>
2016-01-27 12:42:51 +0100

    Added nelmio api doc

--- src/JdhmApi/Controller/DefaultController.php
+++ src/JdhmApi/Controller/DefaultController.php
@@ -7,21 +7,45 @@ use Symfony\Component\HttpFoundation\Request;
 use FOS\RestBundle\Controller\FOSRestController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration as Extra;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
 class DefaultController extends FOSRestController
 {
     /**
-     * @Extra\Route("/", name="homepage")
-     * @Extra\Method({"GET"})
-     */
+    * This method will return the data for the home page
+    *
+    * @ApiDoc(
+    *  section="Home Page",
+    *  resource=true,
+    *  description="This method will return all the posts",
+    *  statusCodes={
+    *      200="Returned when successful",
+    *      403="Returned when the user is not authorized",
+    *      404={
+    *        "Returned when the posts are not found"
+    *      }
+    * }
+    * )
+    * @Extra\Route("/", name="homepage")
+    * @Extra\Method({"GET"})
+    */
     public function indexAction(Request $request)
     {
         $date = new \DateTime();
         $data = [
             'status' => 'Ok',
             'date'  => $date->format("Y-m-d H:i:s"),
-            'message' => "API in developpement :)"
+            'content-type' => $request->getContentType(),
+            'message' => "API in developpement :)",
+            'client' => [
+                'ip' => $request->getClientIp(),
+                'user' => $request->getUser(),
+                'host' => $request->getHttpHost(),
+                'is_secure' =>$request->isSecure()
+
+            ]
         ];
+        //var_dump(get_class_methods($request));
 
         return $this->view()
                     ->setStatusCode(200)
